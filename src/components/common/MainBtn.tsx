df38e08b649e2c58d4c4eeae14cb62cb2e9332d2
ychanwoo <chxan02@naver.com>
2026-01-31 22:11:37 +0900

    fix: install clsx package and replace MainButton button with Link

--- src/components/common/MainBtn.tsx
+++ src/components/common/MainBtn.tsx
@@ -1,11 +1,22 @@
+import Link from "next/link";
+import clsx from "clsx";
+
 interface MainBtnProps {
   children: React.ReactNode;
+  href: string;
+  className?: string;
 }
 
-export default function MainBtn({ children }: MainBtnProps) {
+export default function MainBtn({ children, href, className }: MainBtnProps) {
   return (
-    <button className="text-white text-base w-75 h-12.5 rounded-2xl bg-[#1E7F4F] flex items-center justify-center hover:bg-[#196e43]">
-      {children}
-    </button>
+    <Link
+      href={href}
+      className={clsx(
+        "text-white text-base w-75 h-12.5 rounded-2xl bg-[#1E7F4F] flex items-center justify-center hover:bg-[#196e43]",
+        className,
+      )}
+    >
+      <p>{children}</p>
+    </Link>
   );
 }
