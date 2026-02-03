f8c6a0a4716c6a7fe7c6532a52a90628a4b5a67d
ychanwoo <chxan02@naver.com>
2026-02-03 18:22:27 +0900

    fix: remove text-base style from NextBtn component

--- src/components/common/NextBtn.tsx
+++ src/components/common/NextBtn.tsx
@@ -6,7 +6,7 @@ interface NextBtnProps {
 export default function NextBtn({ children, onClick }: NextBtnProps) {
   return (
     <button
-      className="text-white text-base w-20 h-8.75 rounded-2xl bg-[#1E7F4F] flex items-center justify-center hover:bg-[#196e43]"
+      className="text-white w-20 h-8.75 rounded-2xl bg-[#1E7F4F] flex items-center justify-center hover:bg-[#196e43]"
       onClick={onClick}
     >
       {children}
