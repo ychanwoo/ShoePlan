b24abb8e4d8ce58bbdc73a67c435ed3267289e7d
ychanwoo <chxan02@naver.com>
2026-02-11 14:23:48 +0900

    ui: create pages related to legal (privacy policy, terms of service, contact)

---
+++ src/app/(legal)/policy/page.tsx
@@ -0,0 +1,17 @@
+import HeaderBar from "@/components/common/HeaderBar";
+import TabBar from "@/components/common/TabBar";
+
+export default function PolicyPage() {
+  return (
+    <>
+      <HeaderBar title="Policy" />
+      <div className="text-[#CBD5E1]">Privacy Policy</div>
+      <div className="text-[#CBD5E1]">Privacy Policy</div>
+      <div className="text-[#CBD5E1]">Privacy Policy</div>
+      <div className="text-[#CBD5E1]">Privacy Policy</div>
+      <div className="text-[#CBD5E1]">Privacy Policy</div>
+      <div className="text-[#CBD5E1]">Privacy Policy</div>
+      <TabBar />
+    </>
+  );
+}
