7537d9632a11dbd8520c8878c1dc2a9505e903fc
ychanwoo <chxan02@naver.com>
2026-02-13 17:04:42 +0900

    feat: implement Naver login

---
+++ src/app/auth/naver/callback/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
+export default function NaverCallbackPage() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const code = searchParams.get("code");
+    const state = searchParams.get("state");
+
+    if (!code) return;
+
+    const login = async () => {
+      const res = await fetch("/api/naver-login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ code, state }),
+      });
+
+      if (res.ok) {
+        router.replace("/home");
+      } else {
+        alert("네이버 로그인 실패");
+      }
+    };
+
+    login();
+  }, [searchParams, router]);
+
+  return <div>네이버 로그인 처리중...</div>;
+}
---
+++ src/app/home/HomeClient.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import HeaderBar from "@/components/common/HeaderBar";
+import TabBar from "@/components/common/TabBar";
+import ShoeLifeProgress from "@/components/homeTab/ShoeLifeProgress";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeaderBar title="Home" showInfo />
+
+      <div className="h-[calc(100vh-11vh)] overflow-y-auto pb-100">
+        <div className="flex flex-col items-center relative top-25">
+          <ShoeLifeProgress percentage={72} />
+        </div>
+
+        <div className="text-[#CBD5E1] text-center relative top-40">
+          <p className="font-light">아직 사용하기 좋아요</p>
+          <p className="text-2xl">교체 권장 까지 {130}일 남음</p>
+        </div>
+
+        <div className="text-[#CBD5E1] text-center space-y-3 font-light relative top-55">
+          <p>
+            총 권장 수명: <span>{500}km</span>
+          </p>
+          <p>
+            현재 주행 거리: <span>{160}km</span>
+          </p>
+        </div>
+      </div>
+
+      <TabBar />
+    </>
+  );
+}
