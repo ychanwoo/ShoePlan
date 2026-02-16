6c0efa733afa87b99427fd7dbbced0449e5d5054
ychanwoo <chxan02@naver.com>
2026-02-16 18:32:34 +0900

    feat: implement preview result page

--- src/app/home/HomeClient.tsx
+++ src/app/home/HomeClient.tsx
@@ -3,29 +3,42 @@
 import HeaderBar from "@/components/common/HeaderBar";
 import TabBar from "@/components/common/TabBar";
 import ShoeLifeProgress from "@/components/homeTab/ShoeLifeProgress";
+import { calculateShoeLife } from "@/utils/calculateShoeLife";
 
-export default function HomePage() {
+export default function HomeClient() {
+  const shoeLife = calculateShoeLife();
+  if (!shoeLife) return null;
+  const remainmingKm = shoeLife.recommendedLife - shoeLife.usedDistance;
   return (
     <>
       <HeaderBar title="Home" showInfo />
 
       <div className="h-[calc(100vh-11vh)] overflow-y-auto pb-100">
         <div className="flex flex-col items-center relative top-25">
-          <ShoeLifeProgress percentage={72} />
+          <ShoeLifeProgress percentage={shoeLife?.usagePercent} />
         </div>
 
         <div className="text-[#CBD5E1] text-center relative top-40">
+          {/* percentage에 따라 문구 조정 */}
           <p className="font-light">아직 사용하기 좋아요</p>
-          <p className="text-2xl">교체 권장 까지 {130}일 남음</p>
+          <p className="text-2xl">
+            교체 권장 까지 {shoeLife.remainingMonths}개월 남음
+          </p>
         </div>
 
         <div className="text-[#CBD5E1] text-center space-y-3 font-light relative top-55">
           <p>
-            총 권장 수명: <span>{500}km</span>
+            총 권장 수명: <span>{shoeLife.recommendedLife}km</span>
           </p>
-          <p>
-            현재 주행 거리: <span>{160}km</span>
-          </p>
+          {shoeLife.usagePercent >= 100 ? (
+            <p>
+              총 달린 거리: <span>{shoeLife.usedDistance}km</span>
+            </p>
+          ) : (
+            <p>
+              남은 주행 거리: <span>{remainmingKm}km</span>
+            </p>
+          )}
         </div>
       </div>
 
