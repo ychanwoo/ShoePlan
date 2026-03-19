866c373116b257b9c71d9df0e8b13a6e6c40a74b
ychanwoo <chxan02@naver.com>
2026-03-19 20:57:48 +0900

    chore: add favicon image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import MobileFrame from "@/components/common/MobileFrame";
 
+export const metadata = {
+  title: "ShoePlan",
+  description: "Personalized Running Shoe life cycle management",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
