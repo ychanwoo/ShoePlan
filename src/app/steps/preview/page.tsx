62e830f19517aa4746c1d47dbc4374da91ea67c9
ychanwoo <chxan02@naver.com>
2026-03-26 23:30:11 +0900

    ui: update ui in preview page

--- src/app/steps/preview/page.tsx
+++ src/app/steps/preview/page.tsx
@@ -108,32 +108,41 @@ export default function PreviewPage() {
       </div>
 
       <div className="pb-10">
-        <div className="text-center relative top-15">
+        <div className="text-center relative top-10">
           <p className="text-[#CBD5E1] text-2xl">현재 러닝화 상태</p>
         </div>
-
-        <div className="relative top-30">
-          <p className="text-white text-2xl font-semibold text-center">
-            {isExceeded ? (
-              <>권장 수명을 초과했습니다</>
-            ) : (
-              <>
-                교체까지 약
-                <span className="text-[#1E7F4F] pl-2 pr-1">
-                  {shoeLife?.remainingMonths}
+        <div className="relative top-15 px-4 w-full max-w-[320px] mx-auto">
+          <div
+            className={`flex items-center justify-center py-3 rounded-2xl border ${
+              isExceeded
+                ? "bg-[#1B242C]/80 border-red-900"
+                : "bg-[#1B242C]/80 border-[#1E7F4F]"
+            }`}
+          >
+            <p className="text-[#CBD5E1] text-base font-medium flex items-center justify-center gap-1">
+              {isExceeded ? (
+                <span className="text-rose-700 font-bold">
+                  권장 수명을 초과했습니다
                 </span>
-                개월
-              </>
-            )}
-          </p>
+              ) : (
+                <>
+                  교체까지 약
+                  <span className="text-[#22c55e] text-2xl font-bold px-1 tracking-tight mb-1">
+                    {shoeLife?.remainingMonths}
+                  </span>
+                  <span>개월</span>
+                </>
+              )}
+            </p>
+          </div>
         </div>
 
-        {/* 러닝화 추천 이미지 */}
-        <div>
-          <p className="text-[#CBD5E1] text-center relative top-58">
+        <div className="relative top-30 flex flex-col items-center px-4">
+          <p className="text-[#CBD5E1] text-center mb-6">
             당신의 러닝 스타일에 딱 맞는 러닝화 추천
           </p>
-          <div className="relative overflow-hidden w-45 aspect-square mx-auto top-60">
+
+          <div className="relative overflow-hidden w-45 aspect-square rounded-2xl mb-8">
             <Image
               src={`/preview-images/${randomShoe}.webp`}
               alt="shoe-image"
@@ -142,12 +151,31 @@ export default function PreviewPage() {
             />
             {/* Image blur */}
             <div className="absolute inset-x-0 top-1 bottom-3 overflow-hidden backdrop-blur-[7px]" />
+            {/* Lock */}
+            <div className="absolute inset-0 flex items-center justify-center z-10 pointer-events-none">
+              <div className="w-14 h-14 bg-[#1B242C]/80 rounded-full flex items-center justify-center shadow-lg backdrop-blur-md">
+                <span className="text-3xl">🔒</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="text-center w-full mt-5">
+            <h3 className="text-white text-lg font-bold mb-2">
+              나랑 잘 맞는 러닝화는 무엇일까?
+            </h3>
+            <p className="text-[#CBD5E1] text-sm leading-relaxed">
+              기존 러닝화 수명 관리부터 <br />
+              <span className="text-[#22c55e] font-bold">
+                딱 맞는 러닝화 추천
+              </span>
+              까지 한 번에!
+            </p>
           </div>
         </div>
 
         {/* Login CTA */}
-        <MainBtn href="/login" className="mx-auto mt-90">
-          러닝화 관리 시작하기 →
+        <MainBtn href="/login" className="mx-auto relative top-45">
+          3초 만에 시작하고 결과 보기 →
         </MainBtn>
       </div>
     </>
