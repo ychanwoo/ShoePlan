37bc1a848ea7d4285d70453fc43939e166decd1b
ychanwoo <chxan02@naver.com>
2026-04-02 22:54:01 +0900

    ui: add outsole analyze button

---
+++ src/app/shoe/outsole/page.tsx
@@ -0,0 +1,7 @@
+export default function OutsolePage() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">Outsole Page</h1>
+    </div>
+  );
+}
